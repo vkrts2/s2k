07a0eafb74dc6fdcf50f1948180274ef5540937a
vkrts2 <vkrts01@gmail.com>
2025-06-12 20:44:07 +0300

    İlk yükleme

---
+++ src/components/suppliers/print-view.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function PrintView() {
+  return (
+    <div>
+      <h1>Print View</h1>
+      <p>This is a placeholder for the print view of supplier details.</p>
+    </div>
+  );
+} 
