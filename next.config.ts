fe9c60edbc5b2f6326664a3eacc79dc29bab9601
vkrts2 <vkrts01@gmail.com>
2025-08-16 18:55:16 +0300

    İlk yükleme

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,23 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  swcMinify: true,
-  experimental: {
-    optimizeCss: true,
-  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
         port: '',
         pathname: '/**',
       },
