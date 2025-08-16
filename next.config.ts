f25f33d31765b9f59ffd3fff16c5bef3c863edeb
vkrts2 <vkrts01@gmail.com>
2025-08-16 18:50:31 +0300

    İlk yükleme

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,10 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  swcMinify: true,
+  experimental: {
+    optimizeCss: true,
+  },
   images: {
     remotePatterns: [
       {
--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  plugins: {
-    "postcss-import": {},
-    tailwindcss: {},
-    "postcss-preset-env": { stage: 3, preserve: true },
-  },
-};
